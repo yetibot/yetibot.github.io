c91b215e5ee4f23607bc2b8c587a9c21133202c4
Trevor Hartman <thartman@ebay.com>
2019-10-28 14:28:36 -0600

    Upgrade all deps

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,28 @@ module.exports = {
       './resources/templates/themes/devth/js/'),
     filename: 'dist.js'
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+
+    ],
+  },
+
   resolve: {
-    alias: {
-      "react": "preact-compat",
-      "react-dom": "preact-compat"
-    }
   }
 }
